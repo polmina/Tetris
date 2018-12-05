f70446af2540c91d6c813eb6a5b8202621229ec3
X541U <X541U@192.168.4.18>
2018-12-05 18:04:06 +0100

    Punt 2 Fet

--- public_html/js/model/Piece.js
+++ public_html/js/model/Piece.js
@@ -2,17 +2,42 @@ var Piece = function (id, colour, shapes) {
     this.id = id;
     this.colour = colour;
     this.shapes = shapes;
+    this.currentShape = 0;
     this.counter = 0;
+    this.x = 4;
+    this.y = 0;
     
 };
 
 Piece.prototype.moveRight = function () {
-    
+    this.x++;
 };
 Piece.prototype.moveLeft = function(){
-    
+    this.x--;
+};
+Piece.prototype.moveDown = function(){
+    this.y++;
+};
+Piece.prototype.turnLeft = function(){
+    if((this.currentShape-1)<0){
+        this.currentShape=this.shapes.length-1;
+    }
+    else {
+        this.currentShape--;
+    }
+};
+Piece.prototype.turnRight = function(){
+    if((this.currentShape+1)===this.shapes.length){
+        this.currentShape=0;
+    }
+    else {
+        this.currentShape++;
+    }
 };
 Piece.prototype.increaseCounter = function(){
     this.counter++;
+};
+Piece.prototype.getCurrentShape = function(){
+    return this.shapes[this.currentShape];
 };
 
